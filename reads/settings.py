0d37c6b5d2791c8f14892546769dcf739a6473f3
Martin Balfanz <me@martinbalfanz.com>
2016-10-26 22:47:03 +0200

    Add pagination.

--- reads/settings.py
+++ reads/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
+}
