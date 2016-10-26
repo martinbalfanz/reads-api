a9e6961c90e361c3e85b8427a5d171804a9e2ba8
Martin Balfanz <martin.balfanz@minglabs.com>
2016-10-26 16:36:34 +0200

    Add management command to import pinboard backups.

---
+++ reads/bookmarks/management/commands/importpinboard.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from reads.bookmarks.models import Bookmark
+import json
+
+
+class Command(BaseCommand):
+    help = 'Import pinboard json export into database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('file')
+
+    def handle(self, *args, **options):
+        data = json.load(open(options['file']))
+
+        Bookmark.objects.bulk_create([Bookmark(**x) for x in data])
+        print("done.")
